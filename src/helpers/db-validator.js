9fe1cb4b77f72d7aac425c56397223b04653fe5e
Brian Chavarria <bayyand7@gmail.com>
2021-03-23 15:24:23 -0500

    Correcion del problema al crear un usuario,[error email]

--- src/helpers/db-validator.js
+++ src/helpers/db-validator.js
@@ -1,7 +1,11 @@
 const User  = require('../models/user');
 
 const emailExist = async( email = '') => {
-    const ifExist = await User.findOne({email});
+    const ifExist = await User.findOne({
+        where: {
+            email
+        }
+    });
     if( ifExist ){
         throw new Error(`El correo ${ email }, ya existe en la base de datos`);
     }
